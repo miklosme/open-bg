c93e395f51cf1b72a221eacd6449a00114120100
Miklos Megyes <m.miklos05@gmail.com>
2022-02-20 22:37:22 +0100

    init

--- src/components/canvas/Box.jsx
+++ src/components/canvas/Box.jsx
@@ -5,27 +5,78 @@ import { useMachine } from '@xstate/react'
 import { createMachine } from 'xstate'
 
 const toggleMachine =
-    /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoBGqAHgMQAqA8gOKUAyAooqAA6qwCWybqAdoyIYgC0AJgCsOACwBGAMwAOGRIAMSgJzTRS0aIA0IAJ5CpsnKoDsMmQDZVUtTOETVAX2d60GbDlhMAFmAAnMDIqWgYkEBZ2Th4+AQRBK2EcNVFVJNtRKytNCT1DBJlxOQlhOVEHUWEzJQkZKVc3EG5UCDg+DyxcAnjmVg4uXgj4qSdTMTM5KStpMTs8gyFsnClhNSkKmVVROuErV3d0Lu8-QLA+KIHY4aFhaxw91cs0uTNROXyhWxwayZ2JUp2Cz7JqdbAXfoxIageIiKxKB5WJ5FVSvd6fBJSORWUwWIrGYxmSZyRrOIA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoBGqAHgMQAqA8gOKUAyAooqAA6qwCWybqAdoyIYgC0ARgBsAThwAOKQFYADABZZsqYqkAmAMw6ANCACeQ2cOkB2TcKnzZoxfOHyNsgL4v9aDNhywmACzAAJzAyKloGJBAWdk4ePgEEQQ15URxbM2EzRUVxRzMdLX0jBA1U5w0NcQt5J2FxWXy3dxBuVAg4Pk8sXAIE5lYOLl5IhMFs2RwtUS0pM1thRS1xUqKhKRxRTa0zeXFxKdErNTcPdG6ffyCwPmjBuJHELQmrWRmNKVF5MzNP8VWELTSCTZCyLOS2URmE4gLrYG4DWLDUCjRQSSbTWbzRbLUT-EQaRTmTTJaaHb67JouIA */
     createMachine({
-  id: 'toggle',
-  initial: 'box',
-  states: {
-    box: {
-      on: {
-        TOGGLE: {
-          target: '#toggle.sphere',
+        id: 'toggle',
+        initial: 'sphere',
+        states: {
+            box: {
+                on: {
+                    TOGGLE: {
+                        target: '#toggle.sphere',
+                    },
+                },
+            },
+            sphere: {
+                on: {
+                    TOGGLE: {
+                        target: '#toggle.box',
+                    },
+                },
+            },
         },
-      },
-    },
-    sphere: {
-      on: {
-        TOGGLE: {
-          target: '#toggle.box',
+    })
+
+const battleground =
+    /** @xstate-layout N4IgpgJg5mDOIC5QCMCGAXdAbMUBOA9gK4B2EAdOnqgG5h4nkDuqeaZAxMkQJ6KgAHArACW6EQRL8QAD0QBGeQCZyABgAs6gBwBWVToDsATgDMB1cYA0IPgj1HyR1SZPP1SgGwfTAXx-W0TBx8YjJKajoGZlZ2CA4BLFRbQWExCSkkWQVVByULI3kDAx1tDxNla1t3ByN3Ey0PHQ91J3kPPwCMbFxCUgoqWnpGFjZUTjwwADMJ2AALaSFRcUlpOQR5Z3J1fU1NVQ8lIz0lSsQTHRrijw2c861SjpBA7pC+8MGokdiOabAwAC8wAtUssMqA1kYDORzOV5CYlAYzOodCcbIhDlCjFijFpYep5KYLI9nsFemEBpFhjExnFYGAsFhgUt0qtsjpyIV5LotIp8YSjKcEEoXI5sbU4R4tAZPMSuqTQv0IkNoqNxmBWNQSDAmWkVpk1rpyB5VPISscDFotM4dILCqo1KpHfpTUcUXDZUEegr3pSODJYOgMGByKhJuh6OQAEIAVQAmgB9AAiAFEADIAQVjOtBrIQRShJUOLQ0OKMqKqSnUHKc8Ja0o06i8fn8IBIBAgcGkJK9bwpyq+NOzLP1Z2UW0bZraJicLUF5yhBnkyJ09QM3h58g9LzJio+YJAi11+7W1yNJrNqgRlutgshDqdWNUDV05S38re3cmInpECHevBiCGIKugOE6l6uiaBybi23avGQf7HmcJiCpW94mhOFpcjo0F+EAA */
+    createMachine(
+        {
+    id: 'battleground',
+    initial: 'travern',
+    states: {
+        travern: {
+            initial: 'warband',
+            after: {
+                BUY_DELAY: {
+                    target: '#battleground.battlefield',
+                },
+            },
+            states: {
+                warband: {
+                    on: {
+                        buy: {
+                            target: '#battleground.travern.warband',
+                        },
+                        play: {
+                            target: '#battleground.travern.warband',
+                        },
+                        refresh: {
+                            target: '#battleground.travern.warband',
+                        },
+                        freeze: {
+                            target: '#battleground.travern.warband',
+                        },
+                        sell: {
+                            target: '#battleground.travern.warband',
+                        },
+                        rearrange: {
+                            target: '#battleground.travern.warband',
+                        },
+                    },
+                },
+            },
         },
-      },
+        battlefield: {},
     },
-  },
-})
+},
+        {
+            delays: {
+                BUY_DELAY: (context, event) => {
+                    return context.trafficLevel === 'low' ? 1000 : 3000
+                },
+            },
+        },
+    )
 
 const BoxComponent = ({ route }) => {
     const router = useStore((s) => s.router)
